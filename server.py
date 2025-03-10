de493200707ec619941b2709c9ebf505cff8f9b6
wilsonjmgit <wilsonjm@us.ibm.com>
2025-03-10 18:59:37 -0400

    Updated server.py for Task 6

--- server.py
+++ server.py
@@ -15,12 +15,11 @@ def render_index_page():
 def emotions_detector():
     text_to_analyze = request.args.get('textToAnalyze')
     emotions_dict = emotion_detector(text_to_analyze)
-    dominant_emotion = emotions_dict['dominant_emotion']
-    emotions_text = f"For the given statement, the system response is {emotions_dict}.  The dominant emotion is {dominant_emotion}."
-    if emotions_dict is None:
-        return "Invalid text! Please try again."
-    else:
-        return f"The given text has been identified as {emotions_text}."
+    # Get the dominant emotion, and remove it from emotions_dict
+    dominant_emotion = emotions_dict.pop('dominant_emotion', None)
+    no_brackets_text = str(emotions_dict).replace(r'{','').replace(r'}','')
+    emotions_text = f"For the given statement, the system response is {no_brackets_text}.  The dominant emotion is {dominant_emotion}."
+    return emotions_text
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
