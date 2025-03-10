18ddcfbc9ec1ddaac89131e2918324014ffe762e
wilsonjmgit <wilsonjm@us.ibm.com>
2025-03-10 17:39:45 -0400

    Updates through Task 5

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,16 @@
+import requests, json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    # The value being returned must be the text attribute of the response object as received from the Emotion Detection function.
+    response = requests.post(url, json = myobj, headers=header)
+    formatted_response = json.loads(response.text)
+    emotion_predictions = formatted_response['emotionPredictions'][0]['emotion']
+    # Example emotion_predictions = {'anger': 0.029103195, 'disgust': 0.0067921067, 'fear': 0.027528232, 'joy': 0.876574, 'sadness': 0.06151191}
+    # emotion_predictions is already a dictionary similar to the requested output format.  Just need to add dominant_emotion.
+    dominant_emotion = max(emotion_predictions, key = emotion_predictions.get)
+    # Add dominant_emotion
+    emotion_predictions['dominant_emotion'] = dominant_emotion
+    return emotion_predictions
--- emotion_detection.py
+++
@@ -1,9 +0,0 @@
-import requests, json
-
-def emotion_detector(text_to_analyze):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    myobj = { "raw_document": { "text": text_to_analyze } }
-    # The value being returned must be the text attribute of the response object as received from the Emotion Detection function.
-    response = requests.post(url, json = myobj, headers=header)
-    return response.text
