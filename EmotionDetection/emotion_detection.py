a02d111f9a9b13c80345d020a88fdd1b38047869
wilsonjmgit <wilsonjm@us.ibm.com>
2025-03-10 21:06:42 -0400

    Final files for Developing AI Applications with Python and Flask

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,10 +7,14 @@ def emotion_detector(text_to_analyze):
     # The value being returned must be the text attribute of the response object as received from the Emotion Detection function.
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
-    emotion_predictions = formatted_response['emotionPredictions'][0]['emotion']
-    # Example emotion_predictions = {'anger': 0.029103195, 'disgust': 0.0067921067, 'fear': 0.027528232, 'joy': 0.876574, 'sadness': 0.06151191}
-    # emotion_predictions is already a dictionary similar to the requested output format.  Just need to add dominant_emotion.
-    dominant_emotion = max(emotion_predictions, key = emotion_predictions.get)
-    # Add dominant_emotion
-    emotion_predictions['dominant_emotion'] = dominant_emotion
-    return emotion_predictions
+    if response.status_code == 200:
+        emotion_predictions = formatted_response['emotionPredictions'][0]['emotion']
+        # Example emotion_predictions = {'anger': 0.029103195, 'disgust': 0.0067921067, 'fear': 0.027528232, 'joy': 0.876574, 'sadness': 0.06151191}
+        # emotion_predictions is already a dictionary similar to the requested output format.  Just need to add dominant_emotion.
+        dominant_emotion = max(emotion_predictions, key = emotion_predictions.get)
+        emotion_predictions['dominant_emotion'] = dominant_emotion
+        return emotion_predictions
+    elif response.status_code == 400:
+        return {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': None}
+    else:
+        return None
--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+'''
+server.py to run a server to input textToAnalyze and output emotion analysis response
+'''
 # Import libraries
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
@@ -7,19 +10,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_index_page():
-    # return render_template("index.html", emotion_detector(text_to_analyze))    
-    return render_template("index.html")    
+    '''
+    Function to render index.html page
+    '''
+    return render_template("index.html")
 
 @app.route("/emotionDetector")
 # Need a different name, not 'emotion_detector' so it doesn't clash
 def emotions_detector():
+    '''
+    Function to call emotion_detector with argument 'textToAnalyze',
+    return the emotions dictionary emotions_dict, and
+    format the output as requested. 
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
     emotions_dict = emotion_detector(text_to_analyze)
     # Get the dominant emotion, and remove it from emotions_dict
     dominant_emotion = emotions_dict.pop('dominant_emotion', None)
     no_brackets_text = str(emotions_dict).replace(r'{','').replace(r'}','')
-    emotions_text = f"For the given statement, the system response is {no_brackets_text}.  The dominant emotion is {dominant_emotion}."
-    return emotions_text
-    
+    emotions_text = f"For the given statement, the system response is {no_brackets_text}.\
+      The dominant emotion is {dominant_emotion}."
+    if dominant_emotion is not None:
+        return emotions_text
+    return "Invalid text! Please try again!"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
